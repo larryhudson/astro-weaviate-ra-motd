9715f8a0bc49f430cb86ccab3fffc7e3ddd6e0bc
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2024-01-21 13:27:25 +1100

    adjust ingest chunking

--- src/lib/weaviate/ingest_batch.ts
+++ src/lib/weaviate/ingest_batch.ts
@@ -1,28 +1,36 @@
 import { weaviateClient } from ".";
+import chunkify from "chunkify";
 
-export async function ingestBatch(mixes: any[]) {
+export async function ingestBatch(allMixes: any[]) {
 
   // this was on this page: https://weaviate.io/developers/weaviate/manage-data/import
   let batcher = weaviateClient.batch.objectsBatcher();
 
-  for (const mix of mixes) {
-    const dataObj = {
-      mixId: mix.id,
-      imageUrl: mix.imageUrl,
-      date: mix.date,
-      title: mix.title,
-      blurb: mix.blurb,
-      contentUrl: mix.contentUrl,
-      content: mix.content,
-      textForEmbedding: mix.textForEmbedding,
-      iframe: mix.iframe,
-      hyperlinkUrl: mix.hyperlinkUrl,
-    }
-    batcher = batcher.withObject({
+  const mixChunks = chunkify(allMixes, 100);
+
+
+  for (const chunk of mixChunks) {
+    console.log("chunk", chunk.length)
+    for (const mix of chunk) {
+
+      const dataObj = {
+        mixId: mix.id,
+        imageUrl: mix.imageUrl,
+        date: mix.date,
+        title: mix.title,
+        blurb: mix.blurb,
+        contentUrl: mix.contentUrl,
+        content: mix.content,
+        textForEmbedding: mix.textForEmbedding,
+        iframe: mix.iframe,
+        hyperlinkUrl: mix.hyperlinkUrl,
+      }
+      batcher = batcher.withObject({
         class: "Mix",
         properties: dataObj,
-    })
-  }
+      })
+    }
 
-  await batcher.do();
+    await batcher.do();
+  }
 }
