5e12a6e3d95c42f85ae2bd462d8f2645574286c0
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2024-01-21 14:03:43 +1100

    change batching

--- ingest.ts
+++ ingest.ts
@@ -1,11 +1,9 @@
 import fs from "fs";
-import { ingestBatch } from "@src/lib/weaviate/ingest_batch";
+import { importMixesFromJson } from "@src/lib/weaviate/ingest_batch";
 
 async function main() {
     const jsonFilePath = "./data.json"
-    const jsonStr = fs.readFileSync(jsonFilePath, "utf8");
-    const mixes = JSON.parse(jsonStr);
-    await ingestBatch(mixes);
+    await importMixesFromJson(jsonFilePath);
 }
 
 main();
--- src/lib/weaviate/ingest_batch.ts
+++ src/lib/weaviate/ingest_batch.ts
@@ -1,37 +1,62 @@
 import { weaviateClient } from ".";
 import chunkify from "chunkify";
+import fs from 'fs';
+import parser from 'stream-json';
+import StreamArray from 'stream-json/streamers/StreamArray';
+import Chain from 'stream-chain';
 
-export async function ingestBatch(allMixes: any[]) {
+export async function importMixesFromJson(filePath) {
+  let batcher = weaviateClient.batch.objectsBatcher();
+  let counter = 0;
+  const batchSize = 20;
 
-  // this was on this page: https://weaviate.io/developers/weaviate/manage-data/import
-
-  const mixChunks = chunkify(allMixes, 100);
-
-
-  for (const chunk of mixChunks) {
-    console.log("chunk", chunk.length)
-    let batcher = weaviateClient.batch.objectsBatcher();
-    for (const mix of chunk) {
-
-      const dataObj = {
-        mixId: mix.id,
-        imageUrl: mix.imageUrl,
-        date: mix.date,
-        title: mix.title,
-        blurb: mix.blurb,
-        contentUrl: mix.contentUrl,
-        content: mix.content,
-        textForEmbedding: mix.textForEmbedding,
-        iframe: mix.iframe,
-        hyperlinkUrl: mix.hyperlinkUrl,
-      }
-      batcher = batcher.withObject({
-        class: "Mix",
-        properties: dataObj,
-      })
-
+  async function addMix(mix: object): Promise<void> {
+    const dataObj = {
+      mixId: mix.id,
+      imageUrl: mix.imageUrl,
+      date: mix.date,
+      title: mix.title,
+      blurb: mix.blurb,
+      contentUrl: mix.contentUrl,
+      content: mix.content,
+      textForEmbedding: mix.textForEmbedding,
+      iframe: mix.iframe,
+      hyperlinkUrl: mix.hyperlinkUrl,
     }
+    // Add the object to the batch queue
+    batcher = batcher.withObject({
+      class: 'Mix',
+      properties: dataObj,
+    });
+    counter++;
 
+    // When the batch counter reaches batchSize, push the objects to Weaviate
+    if (counter % batchSize === 0) {
+      // Flush the batch queue and restart it
+      const response = await batcher.do();
+      batcher = weaviateClient.batch.objectsBatcher();
+
+      // Handle errors
+      for (const r of response)
+        if (r.result.errors)
+          throw r.result.errors;
+
+      console.log(`Imported ${counter} articles...`);
+    }
+  }
+
+  const pipeline = new Chain([
+    fs.createReadStream(filePath),
+    parser(),
+    new StreamArray(),
+  ]);
+
+  for await (const { value } of pipeline) {
+    await addMix(value);
+  }
+
+  // Flush any remaining objects
+  if (batcher.payload().objects.length > 0) {
     await batcher.do();
   }
 }
